3f0fc6d5f106c52a44a44602fcd266baca4a1c54
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-10-03 00:33:12 +0200

    Update xChip.ts

--- xChip.ts
+++ xChip.ts
@@ -3,7 +3,7 @@ namespace xChip {
     //%blockId=xChip_begin
     //%block="xChip begin"
     //%shim=xChip::begin
-    export function begin(var: number): number {
+    export function begin(var_: number): number {
         // return is for simulator only. return of cpp code will run on hardware.
         return 1;
     }
