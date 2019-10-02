03f06bf6a7b80233e6deeb08053cc200c513808d
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-10-03 00:24:00 +0200

    Create xchip.ts

---
+++ xchip.ts
@@ -0,0 +1,10 @@
+//% color=#444444 icon="\f0b2"
+namespace xChip {
+    //%blockId=xChip_begin
+    //%block="xChip begin"
+    //%shim=xChip::begin
+    export function begin(var: number): number {
+        // return is for simulator only. return of cpp code will run on hardware.
+        return 1;
+    }
+}
