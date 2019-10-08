3c5a68ad5ed45c033f19c18cbcb53a6fad4a3d59
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-10-08 13:57:36 +0200

    Update xChip.cpp

--- xChip.cpp
+++ xChip.cpp
@@ -4,16 +4,80 @@ using namespace pxt;
 using namespace xChip;
 
 //MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
+  auto sda = LOOKUP_PIN(SDA);
+  auto scl = LOOKUP_PIN(SCL);
+  codal::I2C* i2c = pxt::getI2C(sda, scl);
 
 xChipClass::xChipClass()
 {
-  sda = LOOKUP_PIN(SDA);
-  scl = LOOKUP_PIN(SCL);
+
 }
 
 uint8_t xChipClass::begin(uint8_t var)
 {
   return var; 
+}
+
+bool xSL06::wireWriteByte(uint8_t val)
+{
+    int error;
+    error = i2c->write((uint8_t)APDS9960_I2C_ADDR, (const char*)&val, 1);
+    if(error != 0) {
+        return false;
+    } 
+    return true;
+}
+
+bool xSL06::wireWriteDataByte(uint8_t reg, uint8_t val)
+{
+    int error;
+    uint8_t buf[2];
+    buf[0]=(uint8_t)reg;
+    buf[1]=(uint8_t)val;
+    error = i2c->write((uint8_t)APDS9960_I2C_ADDR, (const char*)&buf, 2);
+    if(error != 0) {
+        return false;
+    } 
+    return true;
+}
+
+bool xSL06::wireWriteDataBlock(uint8_t reg, uint8_t *val, unsigned int len)
+{
+    return true;
+}
+
+bool xSL06::wireReadDataByte(uint8_t reg, uint8_t &val)
+{
+    int error;
+    error = i2c->write((uint8_t)APDS9960_I2C_ADDR, (const char*)&reg, 1, true);
+    if(error != 0) {
+        return false;
+    }
+    error = i2c->read((uint8_t)APDS9960_I2C_ADDR, (char*)&val, 1);
+    if(error != 0) {
+        return false;
+    }
+     return true;
+}
+
+int xSL06::wireReadDataBlock(uint8_t reg, uint8_t *val, unsigned int len)
+{
+    unsigned char i = 0;
+    int error;
+    error = i2c->write((uint8_t)APDS9960_I2C_ADDR, (const char*)&reg, 1, true);
+    if(error != 0) {
+        return false;
+    }
+    error = i2c->read((uint8_t)APDS9960_I2C_ADDR, (char*)val, len);
+    if(error != 0) {
+        return false;
+    }
+    return len;
+}
+
+void xSL06::delay(uint16_t time)
+{
+    //uBit.sleep(time);
 }
 
 // create functions for the .ts file to access
