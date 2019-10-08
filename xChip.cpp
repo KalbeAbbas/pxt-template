31eb4ff77b1d941e9a9b919029831f1593303177
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-10-08 12:11:30 +0200

    Update xChip.cpp

--- xChip.cpp
+++ xChip.cpp
@@ -15,6 +15,64 @@ uint8_t xChipClass::begin(uint8_t var)
   return var; 
 }
 
+bool xChipClass::wireWriteByte(uint8_t val)
+{
+    int error;
+    error = i2c.write((uint8_t)APDS9960_I2C_ADDR, (const char*)&val, 1);
+    if(error != 0) {
+        return false;
+    } 
+    return true;
+}
+
+bool xChipClass::wireWriteDataByte(uint8_t reg, uint8_t val)
+{
+    int error;
+    uint8_t buf[2];
+    buf[0]=(uint8_t)reg;
+    buf[1]=(uint8_t)val;
+    error = i2c.write((uint8_t)APDS9960_I2C_ADDR, (const char*)&buf, 2);
+    if(error != 0) {
+        return false;
+    } 
+    return true;
+}
+
+bool xChipClass::wireWriteDataBlock(uint8_t reg, uint8_t *val, unsigned int len)
+{
+    return true;
+}
+
+bool xChipClass::wireReadDataByte(uint8_t reg, uint8_t &val)
+{
+    int error;
+    error = i2c.write((uint8_t)APDS9960_I2C_ADDR, (const char*)&reg, 1, true);
+    if(error != 0) {
+        return false;
+    }
+    error = i2c.read((uint8_t)APDS9960_I2C_ADDR, (char*)&val, 1);
+    if(error != 0) {
+        return false;
+    }
+     return true;
+}
+
+int xChipClass::wireReadDataBlock(uint8_t reg, uint8_t *val, unsigned int len)
+{
+    unsigned char i = 0;
+    int error;
+    error = i2c.write((uint8_t)APDS9960_I2C_ADDR, (const char*)&reg, 1, true);
+    if(error != 0) {
+        return false;
+    }
+    error = i2c.read((uint8_t)APDS9960_I2C_ADDR, (char*)val, len);
+    if(error != 0) {
+        return false;
+    }
+    return len;
+}
+
+
 
 // create functions for the .ts file to access
 namespace xChip
