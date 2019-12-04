767b898f00297209259917dda64186f7a45c7b84
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-04 15:32:55 +0200

    Update xChip.cpp

--- xChip.cpp
+++ xChip.cpp
@@ -4,9 +4,7 @@ using namespace pxt;
 using namespace xChip;
 
 //MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
-  auto sda = LOOKUP_PIN(SDA);
-  auto scl = LOOKUP_PIN(SCL);
-  codal::I2C* i2c = pxt::getI2C(sda, scl);
+
 
 xChipClass::xChipClass()
 {
@@ -20,6 +18,9 @@ uint8_t xChipClass::begin(uint8_t var)
 
 bool xChipClass::wireWriteByte(uint8_t val)
 {
+   auto sda = LOOKUP_PIN(SDA);
+ auto scl = LOOKUP_PIN(SCL);
+ codal::I2C* i2c = pxt::getI2C(sda, scl);
     int error;
     error = i2c->write((uint16_t)APDS9960_I2C_ADDR, (uint8_t*)&val, 1);
     if(error != 0) {
@@ -30,6 +31,9 @@ bool xChipClass::wireWriteByte(uint8_t val)
 
 bool xChipClass::wireWriteDataByte(uint8_t reg, uint8_t val)
 {
+   auto sda = LOOKUP_PIN(SDA);
+ auto scl = LOOKUP_PIN(SCL);
+ codal::I2C* i2c = pxt::getI2C(sda, scl);
     int error;
     uint8_t buf[2];
     buf[0]=(uint8_t)reg;
@@ -48,6 +52,9 @@ bool xChipClass::wireWriteDataBlock(uint8_t reg, uint8_t *val, unsigned int len)
 
 bool xChipClass::wireReadDataByte(uint8_t reg, uint8_t &val)
 {
+   auto sda = LOOKUP_PIN(SDA);
+ auto scl = LOOKUP_PIN(SCL);
+ codal::I2C* i2c = pxt::getI2C(sda, scl);
     int error;
     error = i2c->write((uint16_t)APDS9960_I2C_ADDR, (uint8_t*)&reg, 1, true);
     if(error != 0) {
@@ -62,6 +69,9 @@ bool xChipClass::wireReadDataByte(uint8_t reg, uint8_t &val)
 
 int xChipClass::wireReadDataBlock(uint8_t reg, uint8_t *val, unsigned int len)
 {
+   auto sda = LOOKUP_PIN(SDA);
+ auto scl = LOOKUP_PIN(SCL);
+ codal::I2C* i2c = pxt::getI2C(sda, scl);
     unsigned char i = 0;
     int error;
     error = i2c->write((uint16_t)APDS9960_I2C_ADDR, (uint8_t*)&reg, 1, true);
