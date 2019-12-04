a255a5090969ed1eae1a7217ac747a231b5e2fbe
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-04 23:32:17 +0200

    Update xChip.ts

--- xChip.ts
+++ xChip.ts
@@ -1,8 +1,1 @@
-//% color=#444444 icon="\f0b2"
-namespace xChip {
 
-    export function begin(var_: number): number {
-        // return is for simulator only. return of cpp code will run on hardware.
-        return 1;
-    }
-}
