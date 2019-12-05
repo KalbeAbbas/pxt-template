fb26c4122a625ca8e9918b736d042ca347998b26
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-05 11:10:12 +0200

    Update xChip.ts

--- xChip.ts
+++ xChip.ts
@@ -1,1 +1,13 @@
-
+/**
+ * xChip c++ template
+ */
+//% weight=99 color=#000000 icon="\uf085"
+namespace xChip {
+    //%blockId=xChip_begin
+    //%block="xChip begin"
+    //%shim=xChip::begin
+    export function begin(var_: number): number {
+        return 1;
+    }
+}
+  
