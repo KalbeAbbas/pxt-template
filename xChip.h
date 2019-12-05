ea392368f3aa0b376d4468cda9ed51d3f6748bd2
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-05 11:17:25 +0200

    Update xChip.h

--- xChip.h
+++ xChip.h
@@ -1,1 +1,19 @@
+#ifndef xChip_h
+#define xChip_h
+#include "pxt.h"
+//#include "MicroBitI2C.h"
+#include "Pin.h"
+#include "I2C.h"
+#ifndef byte
+typedef uint8_t byte;
+#endif
 
+#define APDS9960_I2C_ADDR 0x72
+class myClass
+{
+public:
+    myClass();
+    int begin(int var);
+    uint8_t readDeviceID(uint8_t deviceAddress, uint8_t deviceIDReg);
+};
+#endif
