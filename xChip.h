fce02e4abcbeabe844bf8c70e9e1a02ff7267725
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-05 11:52:13 +0200

    Update xChip.h

--- xChip.h
+++ xChip.h
@@ -8,13 +8,12 @@
 typedef uint8_t byte;
 #endif
 
-#define APDS9960_I2C_ADDR 0x72
+#define addr 0x72
 class myClass
 {
 public:
     myClass();
     int begin(int var);
     uint8_t readDeviceID(uint8_t deviceIDReg);
-    uint8_t addr;
 };
 #endif
